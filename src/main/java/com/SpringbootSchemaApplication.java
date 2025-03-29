d692757d97e19b7bd9ff13430afcba70bc64d682
kauoy <qianqian2786@gmail.com>
2025-03-29 22:12:27 +0800

    first commit

---
+++ src/main/java/com/SpringbootSchemaApplication.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.dao"})
+public class SpringbootSchemaApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(SpringbootSchemaApplication.class, args);
+		String url = "http://localhost:8080/chemical/front/dist/index.html";
+		Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(SpringbootSchemaApplication.class);
+    }
+}
---
+++ src/main/java/com/service/BangzhuService.java
@@ -0,0 +1,37 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.BangzhuEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.BangzhuVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.BangzhuView;
+
+
+/**
+ * 帮助
+ *
+ * @author 
+ * @email 
+ * @date 2025-03-18 17:47:55
+ */
+public interface BangzhuService extends IService<BangzhuEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+    
+   	List<BangzhuVO> selectListVO(Wrapper<BangzhuEntity> wrapper);
+   	
+   	BangzhuVO selectVO(@Param("ew") Wrapper<BangzhuEntity> wrapper);
+   	
+   	List<BangzhuView> selectListView(Wrapper<BangzhuEntity> wrapper);
+   	
+   	BangzhuView selectView(@Param("ew") Wrapper<BangzhuEntity> wrapper);
+   	
+   	PageUtils queryPage(Map<String, Object> params,Wrapper<BangzhuEntity> wrapper);
+   	
+
+}
+
